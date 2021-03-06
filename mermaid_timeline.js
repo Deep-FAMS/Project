06346c367b0ed241a0535f801758400c89ea1b44
Alyetama <56323389+Alyetama@users.noreply.github.com>
2021-03-05 22:22:46 -0500

    update

---
+++ mermaid_timeline.js
@@ -0,0 +1,18 @@
+gantt
+dateFormat  YYYY-DD-MM
+title Project Timeline
+
+section Tasks
+Install libraries & download data:a1, 2021-08-03, 1d
+Preprocess data:des2, 2021-09-03, 1d
+Create initial models:des3, 2021-10-03, 5d
+Test initial models:des4, 2021-15-03, 4d
+Test on METAFACES dataset:des5, 2021-19-03, 8d
+Test on AFHQ dataset  :des6, 2021-19-03, 8d
+Test on CIFAR-10 dataset:des7, 2021-26-03, 8d
+Test on StanfordDogs dataset:des8, 2021-26-03, 8d
+Test on Cars196 dataset:des9, 2021-03-04, 8d
+Test on Ox.Flowers102 dataset:des10, 2021-03-04, 8d
+Evaluate performance:des11, 2021-04-04, 6d
+Statistical analysis comparing our results to the original paper:des12, 2021-10-04, 3d
+Write results and conclusion: des13, 2021-13-04, 10d
